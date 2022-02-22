a248cae85dddcff12bc94cd2a7f5480d3225c2bf
Hrithik Gautam <hrithikgautam81@gmail.com>
2022-02-23 00:31:32 +0530

    time complexity and other recursive functions

---
+++ src/Recursion/factorialRecursion.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class factorialRecursion {
+
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        int smallOutput=fact(n-1);
+        int output=n*smallOutput;
+        return output;
+
+    }
+    public static void main(String[] args){
+        System.out.println(fact(5));
+    }
+}
