2bda32fe5a527ddd919a7f46124b298458c5317e
Hrithik Gautam <hrithikgautam81@gmail.com>
2022-05-03 23:44:47 +0530

    adding queues and other programs

---
+++ src/DataStructures/Queue/Node.java
@@ -0,0 +1,10 @@
+package DataStructures.Queue;
+
+public class Node<T> {
+
+    T data;
+    Node<T> next;
+    Node(T data){
+        this.data=data;
+    }
+}
---
+++ src/DataStructures/Queue/Queues.java
@@ -0,0 +1,26 @@
+package DataStructures.Queue;
+
+public class Queues {
+
+    public static void main(String[] args) {
+
+//        QueueUsingArray queue =  new QueueUsingArray();
+        QueueUsingLL<Integer> queue = new QueueUsingLL<>();
+        int arr[] = {10,20,30,40};
+
+        for(int elem: arr){
+ //       try{
+            queue.enqueue(elem);
+   //     }catch(QueueFullException e){
+     //       }
+
+        }
+        while(!queue.isEmpty()){
+            try{
+                System.out.println(queue.dequeue());
+            }catch (QueueEmptyException e){
+                //will never reach here
+            }
+        }
+    }
+}
